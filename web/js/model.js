38ed894b77d0424b3c41ddef3da74efb3beab188
rob <rob@codebox.org.uk>
2020-05-13 11:13:20 +0100

    validate model

--- web/js/model.js
+++ web/js/model.js
@@ -3,7 +3,8 @@ const modelBuilder = (() => {
 
     const ATTRIB_PREFIX = 'data-doc',
         ATTRIB_CONTAINER = `${ATTRIB_PREFIX}-container`,
-        ATTRIB_ITEM = `${ATTRIB_PREFIX}-id`;
+        ATTRIB_ITEM = `${ATTRIB_PREFIX}-id`,
+        ATTRIB_EXPANDS = `${ATTRIB_PREFIX}-link-expands`;
 
     function findContainers() {
         return Array.from(document.querySelectorAll(`[${ATTRIB_CONTAINER}]`));
@@ -15,20 +16,34 @@ const modelBuilder = (() => {
 
     function buildItem(itemEl) {
         return {
-            id : itemEl.getAttribute(ATTRIB_ITEM)
+            id : itemEl.getAttribute(ATTRIB_ITEM),
+            expands : itemEl.getAttribute(ATTRIB_EXPANDS),
+            content : itemEl.innerText
         }
     }
 
     function buildDocument(containerEl) {
         const items = findItems(containerEl).map(buildItem);
+
         return {
             items
         };
     }
 
+    function validateDocument(doc) {
+        const items = doc.items,
+            itemIdSet = items.map(item => item.id).reduce((s,id) => s.add(id), new Set()),
+            allIdsAreUnique = itemIdSet.size === items.length,
+            allLinksAreValid = items.map(item => item.expands).filter(linkId => linkId).every(linkId => itemIdSet.has(linkId));
+
+        console.log({allIdsAreUnique, allLinksAreValid});
+
+        return allIdsAreUnique && allLinksAreValid;
+    }
+
     return {
         build() {
-            const documents = findContainers().map(buildDocument);
+            const documents = findContainers().map(buildDocument).filter(validateDocument);
 
             return {
                 documents
